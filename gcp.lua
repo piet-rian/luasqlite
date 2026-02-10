cb445245d58ade6a523891727ac6c4c5b7c7eaa0
piet-rian <piet_rian@protonmail.com>
2026-02-10 20:13:44 +0900

    大雑把にじゃんけん結果をファイルに吐き出させるところまで

--- gcp.lua
+++ gcp.lua
@@ -1,35 +1,79 @@
+--- @alias Hand string グー(r)、パー(p)、チョキ(s)のいずれか
+Hand = { r = "r", p = "p", s = "s" }
+
+--- @alias Result string 勝敗結果 "Win"、"Draw"、"Lose"のいずれか
+Result = { winn = "Win", draw = "Draw", lose = "Lose" }
+
 --- じゃんけんの勝敗を判定する関数
--- 引数
---  player1 - プレイヤー1の手 ('r', 'p', 's')
---  player2 - プレイヤー2の手 ('r', 'p', 's
--- 戻り値
---  int - -1: プレイヤー2の勝ち, 0: 引き分け, 1: プレイヤー1の勝ち
+--- @param player1 Hand
+--- @param player2 Hand
+--- @return Result
 function Judge(player1, player2)
   -- 勝敗テーブル
   local win_table = {
-    r = { r =  0, p = -1, s =  1 },
-    p = { r =  1, p =  0, s = -1 },
-    s = { r = -1, p =  1, s =  0 }
+    [Hand.r] = { [Hand.r] = Result.draw, [Hand.p] = Result.lose, [Hand.s] = Result.winn },
+    [Hand.p] = { [Hand.r] = Result.winn, [Hand.p] = Result.draw, [Hand.s] = Result.lose },
+    [Hand.s] = { [Hand.r] = Result.lose, [Hand.p] = Result.winn, [Hand.s] = Result.draw },
   }
 
   return win_table[player1][player2]
 end
 
---- じゃんけんの勝敗メッセージを取得する関数
--- @param result  int
---  result - 勝敗結果 (-1, 0, 1)
--- 戻り値
---  string - 勝敗メッセージ
-function MessageType(result)
-  if result == 1 then
-    return "プレイヤー1の勝ち"
-  elseif result == -1 then
-    return "プレイヤー2の勝ち"
-  else
-    return "引き分け"
-  end
+--- プレイヤーの手をランダムに決定
+--- 手の種類はHand型から選ぶ
+--- @return Hand
+function CreateHand()
+  local hands = { Hand.r, Hand.p, Hand.s }
+  local index = math.random(1, #hands)
+  return hands[index]
 end
 
-local result = Judge('r', 'p')
-local message = MessageType(result)
-print("Message: " .. message) -- 出力: Message: プレイヤー1の
+--[[
+  じゃんけんを行う
+  @return { player1: Hand, player2: Hand, result: Result }
+--]]
+function PlayJanken()
+  local player1_hand = CreateHand()
+  local player2_hand = CreateHand()
+  local result = Judge(player1_hand, player2_hand)
+
+  return {
+    player1 = player1_hand,
+    player2 = player2_hand,
+    result = result
+  }
+end
+
+--- じゃんけんの実行結果をファイルに保存する
+--- @param game_result { player1: Hand, player2: Hand, result: Result }
+--- @param file file*
+--- @return nil
+function SaveJankenResultToFile(game_result, file)
+  local line = JankenResultToString(game_result) .. "\n"
+  file:write(line)
+end
+
+--- じゃんけんの実行結果の文字列表現を生成する
+--- @param game_result { player1: Hand, player2: Hand, result: Result }
+--- @return string
+function JankenResultToString(game_result)
+  local p1 = game_result.player1
+  local p2 = game_result.player2
+  local result = game_result.result
+  return (p1 .. " vs " .. p2 .. " => " .. result)
+end
+
+-- 起動引数からじゃんけんの回数を取得（デフォルトは10回）
+local janken_count = arg[1] and tonumber(arg[1]) or 10
+
+-- じゃんけんを10回実行して結果を表示し、ファイルに保存する
+local result_log_file_name = "janken_results.txt"
+local file = io.open(result_log_file_name, "a")
+
+if file then
+  for _ = 1, janken_count do
+    local game_result = PlayJanken()
+    SaveJankenResultToFile(game_result, file)
+  end
+  file:close()
+end
